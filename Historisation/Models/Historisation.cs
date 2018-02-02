18fa80f87bfa4f009d1d1d6022d01cebf3ea8bd2
tarek rebhi <tarek.rebhi09@gmail.com>
2018-02-02 16:02:11 +0100

    ajout de fichiers

---
+++ Historisation/Models/Historisation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Historisation.Models
+{
+    public class Historisation
+    {
+        public int id { get; set; }
+        public String poles { get; set; }
+        public String sites { get; set; }
+        public String mission { get; set; }
+        public int nombre { get; set; }
+        public String metier { get; set; }
+        public String tache { get; set; }
+        public DateTime d_debut { get; set; }
+        public DateTime d_fin { get; set; }
+        public String duree { get; set; }
+
+    }
+}
---
+++ Historisation/Models/Pause.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Historisation.Models
+{
+    public class Pause
+    {
+        public int id { get; set; }
+        public String type { get; set; }
+        public DateTime h_debut { get; set; }
+        public DateTime h_fin { get; set; }
+    }
+}
